9e0cc1082b19b31aad306494f1b10b67e418b62f
mosalah <mohamedahmedsaleh50@gmail.com>
2023-06-12 08:56:59 -0700

    packages added

--- index.js
+++ index.js
@@ -8,4 +8,41 @@ const questions = [
     message: 'text of Logo'
 },
 {
-    
+    name:'textColor',
+    type: 'input',
+    message: 'textColor'
+},
+{
+    name:'shape',
+    type: 'list',
+    message: 'What is Logo shape?',
+    choices: ['triangle', 'circle', 'square']
+},
+{
+    name:'shapeColor',
+    type: 'input',
+    message: 'shapeColor'
+}
+]
+inquirer.prompt(questions)
+.then(answers => {
+    let shape = null
+    if (answers.shape === 'circle') {
+        shape = new Circle()
+        }
+    else if (answers.shape === 'square') {
+shape = new Square()
+    } else {
+        shape = new Triangle()
+    }
+    shape.setColor(answers.shapeColor) 
+
+    const svg = new Svg()
+    svg.setText(answers.textColor, answers.text)
+    svg.setShape(shape)
+    return writeFile("generatedLogo.svg", svg.render())
+}) .then(() => {
+    console.log("svg generated successfully")
+}) .catch((error) => {
+    console.log(error)
+})
